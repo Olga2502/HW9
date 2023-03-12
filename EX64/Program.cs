d3eba27c5e5851a7f54d5dbcd62275f307750ae7
Olga <101519229+Olga2502@users.noreply.github.com>
2023-03-12 17:26:22 +0300

    Задача 64

---
+++ EX64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+string NaturNumb(int N)
+{
+    if (N == 1) return N.ToString();
+    else
+    {
+        return N.ToString() + ", " + NaturNumb(N -1);
+  
+    }
+}
+Console.WriteLine("Введите число:  ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NaturNumb(N));
+
+
+
+
+
+
+
+
+
+
