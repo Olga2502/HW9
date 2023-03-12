618a799b448ef94db4b20dbfaddbf7ac0157cf1f
Olga <101519229+Olga2502@users.noreply.github.com>
2023-03-12 17:52:48 +0300

    Задача 66

---
+++ EX66/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+
+Console.Clear();
+
+int NaturNumb(int M, int N)
+{
+    if (M == N) return M;
+    else
+    {
+        return NaturNumb(N + 1, M) + N;
+    }
+}
+
+Console.WriteLine("Введите число от :  ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число до:  ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(NaturNumb(M, N));
+
